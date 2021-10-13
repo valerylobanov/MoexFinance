a0c5459fc25c5e52ff6407a970ba9658bd256b2e
Valery Lobanov <valerylobanov@gmail.com>
2021-10-13 22:03:17 +0300

    return headers option

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-/*
- * adds ETF menu to Google Sheets which allows to pull ETF/BPIF data from MOEX to Quotes sheet
- * alternatively use =MOEXFINANCE_ARRAY() function in any cell needed
- *
- * API description
- * https://habr.com/en/post/487436/
- * https://iss.moex.com/iss/reference/
- *
- */
 /**
  * Get quote or other security data from MOEX.
  * Similar to GooglFinance formula.
@@ -16,27 +7,28 @@
  * Other options TRADEDATE SHORTNAME NUMTRADES VALUE OPEN LOW HIGH LEGALCLOSEPRICE WAPRICE CLOSE VOLUME MARKETPRICE2
  * @customfunction
  */
- function MOEXFINANCE(ticker, column = 'CLOSE') {
-
-  // call api for shares / ETFs
-  var rows = getRows(ticker, "TQTF") // ETFs
-  if (rows.length == 0)
+ function MOEXFINANCE(ticker, header = false) {
+  // call api for shares / ETFs; should be one of specified boards
+  // TODO: get board by ticker using api method
+  var rows = getRows(ticker, "TQTF",header) // ETFs
+  if (!rows.length)
     rows = getRows(ticker, "TQBR") // shares
-
-  // convert and return
-  if (column == "CLOSE")
-    return rows
-  else
-    return rows
+  return rows
 }
 
 /*
  * set request url for api described at https://iss.moex.com/iss/reference/817
- * example: https://iss.moex.com/iss/history/engines/stock/markets/shares/sessions/1/boards/TQTF/securities/FXRL.json?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off
+ * example: https://iss.moex.com/iss/history/engines/stock/markets/shares/sessions/1/boards/TQTF/securities/FXRL.xml?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off
  */
-function getRows(ticker, board) {
+function getRows(ticker, board,header) {
   var url = `https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/${board}/securities/${ticker}.json?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off`
   var jsondata = UrlFetchApp.fetch(url);
   var object   = JSON.parse(jsondata.getContentText());
-  return  object.history.data
-}
+
+  // TODO: reurna matrix at once without header parameter
+  if (header)
+    return [object.history.columns]
+  else
+    return object.history.data
+
+  }
