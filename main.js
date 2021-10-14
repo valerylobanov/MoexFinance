d0429ca8bb76c748b39e92fdd16dd053ae5c0f99
Valery Lobanov <valerylobanov@gmail.com>
2021-10-14 22:06:22 +0300

    added date paremeter

--- main.js
+++ main.js
@@ -1,45 +1,42 @@
-/*
- * API description
- * https://habr.com/en/post/487436/
- * https://iss.moex.com/iss/reference/
- * /
-
 /**
- * Get quote or other security data from MOEX.
+ * Get ETF/Stocks data from MOEX. 
+ * Last available historical data is returned.
  * Similar to GooglFinance formula.
  *
  * @param {string} ticker Ticker at MOEX.
- * @param {string} column Column to get, default = CLOSE. 
+ * @param {string} date YYYY-MM-DD format, get data closest (down) to specified date. 
  * Other options TRADEDATE SHORTNAME NUMTRADES VALUE OPEN LOW HIGH LEGALCLOSEPRICE WAPRICE CLOSE VOLUME MARKETPRICE2
  * @customfunction
  */
- function MOEXFINANCE(ticker) {
+function MOEXFINANCE(ticker,date) {
   // call api for shares / ETFs; should be one of specified boards
   // TODO: get board by ticker using api method
-  var rows = getRows(ticker, "TQTF") // ETFs
-  if (!rows)
-    rows = getRows(ticker, "TQBR") // shares
-  return rows
+  var data = fetchData(ticker, "TQTF",date) // ETFs
+  if (!data)
+    data = fetchData(ticker, "TQBR",date) // shares
+  return data
 }
 
 /*
  * set request url for api described at https://iss.moex.com/iss/reference/817
- * example: https://iss.moex.com/iss/history/engines/stock/markets/shares/sessions/1/boards/TQTF/securities/FXRL.xml?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off
+ * example: https://iss.moex.com/iss/history/engines/stock/markets/shares/sessions/1/boards/TQTF/securities/FXRL.json?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off
  */
-function getRows(ticker, board) {
-  
-  var url = `https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/${board}/securities/${ticker}.json?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off`
-  
-  var jsondata = UrlFetchApp.fetch(url);
-  var object   = JSON.parse(jsondata.getContentText());
-  
-  if(!object.history.data.length)
-  return null
+function fetchData(ticker, board, date) {
 
+  var url = `https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/${board}/securities/${ticker}.json?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off`
+  if (date)
+    url = url + `&till=${date}`
+
+  var jsondata = UrlFetchApp.fetch(url);
+  var object = JSON.parse(jsondata.getContentText());
   var res = []
-  for(i=0;i<object.history.columns.length;i++) {
-      res.push([object.history.columns[i],object.history.data[0][i]])
-      
+
+  if (!object.history.data.length)
+    return null
+
+  for (i = 0; i < object.history.columns.length; i++) {
+    res.push([object.history.columns[i], object.history.data[0][i]])
+
   }
   return res
-  }
+}
