09c995f1ca66915246ba74408668b47841ea69d0
Valery Lobanov <valerylobanov@gmail.com>
2021-10-17 13:21:34 +0300

    cpi option added

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*
+ * Google sheet library to enabe MOEXFINANCE custom function
+ * test file: https://docs.google.com/spreadsheets/d/1GFxPdG_oGjfSoxTJvCz-ipNvL_8w2MVyT_tVy5JDyRE/edit?usp=sharing
+ * GitHub repo: https://github.com/valerylobanov/MoexFinance
+ */
+
 /**
  * Get ETF/Stocks data from MOEX. 
  * Last available historical data is returned.
@@ -7,12 +13,15 @@
  * @param {string} date YYYY-MM-DD format, get data closest (down) to specified date. 
  * @customfunction
  */
-function MOEXFINANCE(ticker,date) {
+ function MOEXFINANCE(ticker, date,dateStart) {
+  if (ticker == "CPI:RUB")
+    return fetchGksData(date,dateStart)
+
   // call api for shares / ETFs; should be one of specified boards
   // TODO: get board by ticker using api method
-  var data = fetchData(ticker, "TQTF",date) // ETFs
+  var data = fetchMoexData(ticker, "TQTF", date) // ETFs
   if (!data)
-    data = fetchData(ticker, "TQBR",date) // shares
+    data = fetchMoexData(ticker, "TQBR", date) // shares
   return data
 }
 
@@ -20,7 +29,7 @@ function MOEXFINANCE(ticker,date) {
  * set request url for api described at https://iss.moex.com/iss/reference/817
  * example: https://iss.moex.com/iss/history/engines/stock/markets/shares/sessions/1/boards/TQTF/securities/FXRL.json?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off
  */
-function fetchData(ticker, board, date) {
+function fetchMoexData(ticker, board, date) {
 
   var url = `https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/${board}/securities/${ticker}.json?sort_order=desc&sort_column=TRADEDATE&limit=1&iss.meta=off`
   if (date)
@@ -38,4 +47,14 @@ function fetchData(ticker, board, date) {
 
   }
   return res
+}
+
+/*
+ * set request url for api described at https://www.statbureau.org/ru/inflation-api
+ * example: https://www.statbureau.org/calculate-inflation-rate-jsonp?country=russia&start=2021-01-01&end=2021-08-01
+ */
+function fetchGksData(date, dateStart) {  
+  var url = `https://www.statbureau.org/calculate-inflation-rate-json?country=russia&start=${dateStart}&end=${date}`
+   var jsondata = UrlFetchApp.fetch(url);
+   return parseFloat(jsondata.getContentText().replace('"','').replace('"',''))/100
 }
